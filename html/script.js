df3f52dd78eb894825ac6b4798f49bb573e4f8df
FloppyDisk01 <aurelien.filatre@gmail.com>
2019-02-12 11:33:49 +0100

    ajout variable isOk pour formulaire

--- html/script.js
+++ html/script.js
@@ -11,35 +11,41 @@ function switchLight() {
 }
 
 function validateForm(e) {
+	var isOk = true;
 	var x = document.getElementsByClassName("checkable");
 	var y = document.getElementsByClassName("checkableParticulier");
 	for(i=0;i < x.length; i++){
 		if(x[i].id == "password"){
 			if(x[i].value.length < 6){
+				isOk = false;
 				console.log("mots de passe trop court");
 				e.preventDefault();
 			}
 		}
 		if(x[i].id == "Cpassword"){
 			if(x[i-1].value != x[i].value){
+				isOk = false;
 				console.log("mots de passe différents");
 				e.preventDefault();
 			}
 		}
 		else if(x[i].value.length <= 3){
+			isOk = false;
 			console.log("champs incomplets : " + x[i].id);
 			e.preventDefault();
 		}
 	}
 	for(i=0;i < y.length; i++){
 		if(y[i].id == "age"){
-			if(isNaN(y[i].value) || y[i].value < 0){
+			if(isNaN(y[i].value) || y[i].value < 18){
+				isOk = false;
 				console.log("age entré invalide");
 				e.preventDefault();
 			}
 		}
 		if(y[i].id == "female"){
 			if(!y[i].checked && !y[i-1].checked){
+				isOk = false;
 				console.log("selectionnez un genre");
 				e.preventDefault();
 			}
